75dc803920cc91da8152a15aabf53bf93ea27185
jacky <jacky123789456@gmail.com>
2023-11-06 00:58:48 +0800

    在service新增 增刪改查功能

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
+import { User } from './user/models/user.entity';
 
 @Controller()
 export class AppController {
@@ -21,5 +22,28 @@ export class AppController {
       password: '123456',
       account: 'admin',
     })
+  }
+
+  // 刪除一個用戶
+  @Get('/del')
+  async del(id: string): Promise<boolean> {
+    return await this.userService.del(id);
+  }
+
+  // 更新用戶的資料
+  @Get('/update')
+  async update(): Promise<boolean> {
+    return await this.userService.update(
+      '71e44b16-3376-4513-b8b9-5c3c48ce37b1',
+      {
+        name: '水滴超級管理員1111',
+      },
+    )
+  }
+
+  // 查詢用戶
+  @Get('/find')
+  async find(): Promise<User> {
+    return await this.userService.find('71e44b16-3376-4513-b8b9-5c3c48ce37b1')
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -20,4 +20,36 @@ export class UserService {
     console.log(res);
     return true;
   }
+
+  // 刪除一個用戶
+  async del(id: string): Promise<boolean> {
+    const res = await this.UserRepository.delete(id);
+    if (res.affected > 0) {
+      console.log(res);
+      return true;
+    }
+    return false;
+  }
+
+  // 更新一個用戶
+  async update(id: string, entity: DeepPartial<User>): Promise<boolean> {
+    // update有兩個參數, 第一個是id, 第二個是更改的內容
+    const res = await this.UserRepository.update(id, entity);
+    if (res.affected > 0) {
+      console.log(res);
+      return true;
+    }
+    return false;
+  }
+
+  // 更新一個用戶
+  async find(id: string): Promise<User> {
+    // update有兩個參數, 第一個是id, 第二個是更改的內容
+    const res = await this.UserRepository.findOne({
+      where: {
+        id,
+      }
+    });
+    return res;
+  }
 }
