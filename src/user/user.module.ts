e8e44a4b9e860917bd0ae51dab45dfd055b0d587
jacky <jacky123789456@gmail.com>
2023-11-12 11:35:18 +0800

    新增user.resolver 並使用調適工具

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './models/user.entity';
 import { UserService } from './user.service';
+import { UserResolver } from './user.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [ConsoleLogger, UserService],
+  providers: [ConsoleLogger, UserService, UserResolver],
   exports: [UserService], // 必須在這export出去, app.controller才能調用到UserService
 })
 export class UserModule {}
---
+++ src/user/user.resolver.ts
@@ -0,0 +1,19 @@
+import { Args, Mutation, Resolver, Query } from "@nestjs/graphql";
+import { UserInput } from "./dto/user-input.type";
+import { UserService } from "./user.service";
+import { UserType } from "./dto/user.type";
+
+@Resolver()
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+  
+  @Mutation(() => Boolean) // 新增操作
+  async create(@Args('params') params: UserInput): Promise<boolean> { // 新增參數需要使用@Args的裝飾器
+    return await this.userService.create(params);
+  }
+
+  @Query(() => UserType) // 查詢
+  async find(@Args('id') id: string): Promise<UserType> {
+    return await this.userService.find(id)
+  }
+}
