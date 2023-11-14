31f0133982b99fd3949a10a2b66061c6b0cccaff
jacky <jacky123789456@gmail.com>
2023-11-15 00:17:12 +0800

    GraphQl增加刪除, 更新操作

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -7,7 +7,7 @@ import { UserType } from "./dto/user.type";
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
   
-  @Mutation(() => Boolean) // 新增操作
+  @Mutation(() => Boolean, { description: '新增用戶' }) // 新增操作
   async create(@Args('params') params: UserInput): Promise<boolean> { // 新增參數需要使用@Args的裝飾器
     return await this.userService.create(params);
   }
@@ -15,5 +15,15 @@ export class UserResolver {
   @Query(() => UserType, { description: '使用ID查詢用戶' }) // 查詢
   async find(@Args('id') id: string): Promise<UserType> {
     return await this.userService.find(id);
+  }
+
+  @Mutation(() => Boolean, { description: '更新用戶' })
+  async update(@Args('id') id: string, @Args('params') params: UserInput): Promise<boolean> {
+    return await this.userService.update(id, params);
+  }
+
+  @Mutation(() => Boolean, { description: '刪除一個用戶' })
+  async del(@Args('id') id: string): Promise<boolean> {
+    return this.userService.del(id);
   }
 }
